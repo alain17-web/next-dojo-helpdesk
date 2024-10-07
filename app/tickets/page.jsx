dcc8489e326a5b5d3e43fee313c57eef40bd6baf
alain17-web <web2020.alain@gmail.com>
2024-10-07 12:24:57 +0200

    FINISH tuto 10

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,6 +1,8 @@
 import TicketList from "@/app/tickets/TicketList";
+import {Suspense} from "react";
+import Loading from "@/app/loading";
 
-export default function  Tickets () {
+export default function Tickets() {
     return (
         <main>
             <nav>
@@ -9,7 +11,9 @@ export default function  Tickets () {
                     <p><small>Currently open tickets</small></p>
                 </div>
             </nav>
-            <TicketList />
+            <Suspense fallback={<Loading/>}>
+                <TicketList/>
+            </Suspense>
         </main>
     );
 };
